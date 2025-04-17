793a68bf6a7d7df5ba487159bfecac4cbb8116af
Samaelish <semnd007@gmail.com>
2025-04-17 17:42:44 +0300

    начат перенос на tailwind

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,22 @@
 import { Link } from 'react-router-dom'
-import '../css/NavBar.css'
 
 function NavBar() {
   return (
-    <nav className='navbar'>
-      <div className='navbar-brand'>
+    <nav className='bg-black md:px-8 md:py-4 flex justify-between items-center shadow-[0_2px_4px_rgba(0,0,0,0.1)] p-4'>
+      <div className='font-bold md:text-2xl text-[1.2rem]'>
         <Link to='/'>Игрули</Link>
       </div>
-      <div className='navbar-links'>
-        <Link to='/' className='nav-link'>
+      <div className='flex md:gap-8 gap-4'>
+        <Link
+          to='/'
+          className='text-base md:px-2 md:py-4 transition-colors duration-200 bg-black hover:bg-gray-800 rounded p-2'
+        >
           Главная
         </Link>
-        <Link to='/favorites' className='nav-link'>
+        <Link
+          to='/favorites'
+          className='text-base md:px-2 md:py-4 transition-colors duration-200 bg-black hover:bg-gray-800 rounded p-2'
+        >
           Избранное
         </Link>
       </div>
