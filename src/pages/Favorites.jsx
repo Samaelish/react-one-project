3799851fdd588da69dcdf9ee2a4a649ee820325f
Samaelish <semnd007@gmail.com>
2025-04-18 14:49:06 +0300

    переход на tailwind завершен

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,4 +1,3 @@
-import '../css/Favorites.css'
 import { useGameContext } from '../contexts/GameContext'
 import GameCard from '../components/GameCard'
 
@@ -6,17 +5,23 @@ function Favorites() {
   const { favorites } = useGameContext()
 
   return (
-    <div className={`favorites${favorites.length ? '' : '-empty'}`}>
-      <h2>{favorites.length ? 'Твоё избранное' : 'В избранном пусто. Добавь что-нибудь!'}</h2>
+    <div
+      className={`${
+        favorites.length ? 'p-8 w-full' : 'py-16 px-8 bg-[rgba(255,255,255,0.05)] rounded-xl my-8 mx-auto max-w-[600px]'
+      }`}
+    >
+      <h2 className='text-center text-[2rem] mb-4 text-[#e50914]'>
+        {favorites.length ? 'Твоё избранное' : 'В избранном пусто. Добавь что-нибудь!'}
+      </h2>
 
       {favorites.length > 0 ? (
-        <div className='grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-6 p-6 w-full box-border'>
+        <div className='grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-6 p-6 w-full'>
           {favorites.map(game => (
             <GameCard game={game} key={game.id} />
           ))}
         </div>
       ) : (
-        <p>Начни добавлять в избранное игры и они появятся здесь!</p>
+        <p className='text-[#999] text-xl leading-[1.6]'>Начни добавлять в избранное игры и они появятся здесь!</p>
       )}
     </div>
   )
